3770e88ffa614eb2038ca2281666d57fcd8ebba1
Cyrus <22678716+cyrusdobbs@users.noreply.github.com>
2023-05-30 12:25:28 +0100

    [#PAB-295] connect data-store to postgres db

--- scripts/db-race-conditions-windows-fix.sh
+++ scripts/db-race-conditions-windows-fix.sh
@@ -3,6 +3,7 @@
 docker compose up -d database
 container_id=$(docker ps --format "{{.ID}}" --filter "name=database")
 sleep 1
+docker exec -ti $container_id psql -U postgres -c "CREATE DATABASE data_store;"
 docker exec -ti $container_id psql -U postgres -c "CREATE DATABASE account_store;"
 docker exec -ti $container_id psql -U postgres -c "CREATE DATABASE fund_store;"
 
