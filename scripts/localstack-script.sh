70ee952d1ecf1164e3f9fbc01348715c2e8f9b31
Bobbi Brown <rholdingbrown@gmail.com>
2023-09-25 17:23:36 +0100

    add localstack config

--- scripts/localstack-script.sh
+++ scripts/localstack-script.sh
@@ -1,4 +1,17 @@
+#!/bin/bash
+
 awslocal s3api \
-create-bucket --bucket fmd-data-extract \
+create-bucket --bucket data-store-failed-files-test \
 --create-bucket-configuration LocationConstraint=eu-central-1 \
 --region eu-central-1
+
+awslocal s3api \
+create-bucket --bucket data-store-file-assets-test \
+--create-bucket-configuration LocationConstraint=eu-central-1 \
+--region eu-central-1
+
+ aws s3 cp /tmp/example-template.xlsx s3://data-store-file-assets-test \
+--endpoint-url http://localhost:4566
+
+ aws s3 cp /tmp/example-total-grant-awarded s3://data-store-file-assets-test \
+--endpoint-url http://localhost:4566
