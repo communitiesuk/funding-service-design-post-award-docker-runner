23a86018ee9c0cf84b90e1473b4b0c983e3d4262
Noel Snape <95703578+nsnape@users.noreply.github.com>
2023-11-13 11:10:03 +0000

    [FMD-8] create separate DB instance for tests

--- scripts/db-race-conditions-windows-fix.sh
+++ scripts/db-race-conditions-windows-fix.sh
@@ -4,6 +4,7 @@ docker compose up -d database
 container_id=$(docker ps --format "{{.ID}}" --filter "name=database")
 sleep 1
 docker exec "$container_id" psql -U postgres -c "CREATE DATABASE data_store;"
+docker exec "$container_id" psql -U postgres -c "CREATE DATABASE data_store_test;"
 docker exec "$container_id" psql -U postgres -c "CREATE DATABASE account_store;"
 
 if [[ $* == *--build* ]]
