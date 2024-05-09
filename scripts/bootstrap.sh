993977789ad027497616cc4f882ab4e2d951d4b4
Albert Kolozsvari <albertartk@gmail.com>
2024-05-09 15:21:10 +0100

    Add n1 flag to xargs command
    Add n1 flag to xargs command to make it Linux friendly.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,7 +2,7 @@
 
 echo "Cloning required repositories ..."
 
-for repo in $(cat docker-compose.yml | grep 'context: ../' | cut -d':' -f2 | xargs basename); do
+for repo in $(cat docker-compose.yml | grep 'context: ../' | cut -d':' -f2 | xargs -n1 basename); do
   if ! [ -d "../${repo}" ]; then
     echo "Cloning ${repo} ..."
     git clone git@github.com:communitiesuk/${repo} ../${repo}
